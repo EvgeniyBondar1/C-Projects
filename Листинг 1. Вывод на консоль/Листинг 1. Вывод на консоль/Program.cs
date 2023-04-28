e523664de3d7ad776a7a3ed984c34478ecac32ed
Бондарь Евгений Игоревич <cupe7@rambler.ru>
2023-04-28 23:05:22 +0300

    Листинг 1. Вывод на консоль

---
+++ Листинг 1. Вывод на консоль/Листинг 1. Вывод на консоль/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Листинг_1._Вывод_на_консоль
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Пока, мир!";
+            string name = "Андрей";
+            int age = 37;
+
+            Console.WriteLine("Привет, мир!");
+            Console.WriteLine(msg);
+            Console.WriteLine(100);
+
+            Console.WriteLine($"Имя: {name}; Возраст {age}");
+            Console.WriteLine("Еще один способ:");
+            Console.WriteLine("Имя: {0}; Возраст {1}", name, age);
+
+            Console.ReadLine();
+        }
+    }
+}
