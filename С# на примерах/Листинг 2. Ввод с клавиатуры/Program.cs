4c76d7c52ad55f068aa43bcc8c2c72a9b15c282a
Бондарь Евгений Игоревич <cupe7@rambler.ru>
2023-04-28 23:21:29 +0300

    Листинг 2. Ввод с клавиатуры

---
+++ С# на примерах/Листинг 2. Ввод с клавиатуры/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Листинг_2._Ввод_с_клавиатуры
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя:");
+            string? name = Console.ReadLine();
+
+            Console.Write("Введите возраст:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите рост:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Имя: {name}; Возраст {age}; Рост {height}");
+            Console.ReadLine();
+        }
+    }
+}
