6db556a175251c2a48977f2beb37fc81aab24b14
Бондарь Евгений Игоревич <cupe7@rambler.ru>
2023-04-29 21:49:14 +0300

    Листинг 3. Код калькулятора расхода топлива

---
+++ С# на примерах/Листинг 3. Код калькулятора расхода топлива/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Листинг_3._Код_калькулятора_расхода_топлива
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Простой калькулятор расхода топлива");
+            Console.Write("Введите расстояник (км): ");
+
+            double dist = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Потрачено топлива (л): ");
+            double vol = Convert.ToDouble(Console.ReadLine());
+
+            double cons = (100 * vol) / dist;
+
+            Console.WriteLine($"Автомобиль прошел: {dist} км, потратил {vol} л топлива. Средний расход - {cons} л/100 км");
+            Console.ReadLine();
+        }
+    }
+}
